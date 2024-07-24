3ec4a7178108bf352b25416772eb17a492fcd7be
beachboy <firmanilham19@gmail.com>
2024-07-25 03:33:38 +0700

    [fix] host server

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const connection = require('./connection');
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 5001,
-    host: 'localhost',
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
