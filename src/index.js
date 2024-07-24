725207016630b33eab400809223435ce25ca59c2
beachboy <firmanilham19@gmail.com>
2024-07-25 01:42:22 +0700

    [fix] fix env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
-require('dotenv').config();
 
 const init = async () => {
   const server = Hapi.server({
